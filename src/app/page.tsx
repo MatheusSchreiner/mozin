9e30d06e9a6907e0293d64f593cf4c6b3aacbdc0
matheus.schreiner <matheusrubisch@gmail.com>
2024-06-22 20:41:46 +0100

    Código feito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
+import styles from './styles.module.css';
+
+const images = [
+  '/foto 6.jpg',
+  '/foto 7.jpeg',
+  '/foto 10.jpg',
+  '/foto 12.jpg',
+];
+
 export default function Home() {
   return (
-    <section>
-      Mozin
-    </section>
+    <div className={styles.container}>
+      <div className={styles.heart}></div>
+      <h1 className={styles.title}>TEAMO MOZIN!!!</h1>
+      <div className={styles.imagesContainer}>
+        {images.map((src, index) => (
+          <div key={index} className={styles.imageWrapper}>
+            <Image src={src} alt={`Image ${index + 1}`} width={300} height={300} className={styles.image} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
